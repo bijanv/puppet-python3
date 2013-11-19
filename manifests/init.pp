9949bc6e79965796a23245a3255bb2ec3bbb798c
Bijan Vaez <bijan@eventmobi.com>
2013-11-19 14:08:03 -0500

    Fixing lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
+#Class Python3
 class python3 {
   package { 'Python3':
     provider => 'pkgdmg',
     source   => 'http://www.python.org/ftp/python/3.3.3/python-3.3.3-macosx10.6.dmg',
-    ensure   => installed,
   }
 }
