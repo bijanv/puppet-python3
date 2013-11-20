d9fb3445fc3fd058f5beff1f1678b0d83a1d1f7f
Bijan Vaez <bijan@eventmobi.com>
2013-11-19 19:04:14 -0500

    Adding installation of pip

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,15 @@
 #Class Python3
 class python3 {
+  include homebrew
+
   package { 'Python3':
     provider => 'pkgdmg',
     source   => 'http://www.python.org/ftp/python/3.3.3/python-3.3.3-macosx10.6.dmg',
+    notify   => Exec['install-pip'],
+  }
+
+  exec { 'install-pip':
+    command     => "brew install pip",
+    onlyif      => "pip -v",
   }
 }
--- spec/classes/python3_spec.rb
+++ spec/classes/python3_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'python3' do
   it do
     should contain_class('python3')
+    should include_class('homebrew')
     should contain_package('Python3').with({
       :provider => 'pkgdmg',
       :source   => 'http://www.python.org/ftp/python/3.3.3/python-3.3.3-macosx10.6.dmg'
